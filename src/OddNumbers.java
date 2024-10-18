c66242cf472f45fc3cb4420c4c1b5b0bb75102a7
binhlo <binhlothanh0111@gmail.com>
2024-10-18 11:04:58 +0700

    bai 11

---
+++ src/OddNumbers.java
@@ -0,0 +1,14 @@
+/*Bài 11. Viết chương trình in ra những*/
+
+public class OddNumbers {
+
+    public OddNumbers() {
+        // Khong can tham so truyen vao
+    }
+
+    public void printOddNumbers() {
+        for (int i = 1; i < 100; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
